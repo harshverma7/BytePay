cef99213bc0be7753a714205216c3282e5edf242
Harsh <harshverma7k@gmail.com>
2025-02-05 00:09:13 +0530

    recoil did not supported react 19 so degraded to react 18

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,15 @@
-"use client"
+"use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Appbar } from "@repo/ui/appbar";
+import { useBalance } from "@repo/store/balance";
 
 export default function Page(): JSX.Element {
   const session = useSession();
+  const balance = useBalance();
   return (
-   <div>
+    <div>
       <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
-   </div>
+      <div>{balance}</div>
+    </div>
   );
 }
